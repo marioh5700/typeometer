9100b67129e34d1023088bf6b101a0e8d9e55149
MarioH95 <harb.mario@gmail.com>
2020-01-23 17:38:14 -0400

    changed cors permissions to front end url

--- back-end/index.js
+++ back-end/index.js
@@ -15,7 +15,7 @@ app.use(session({
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:3000'
+    origin: 'https://typeometer.herokuapp.com/'
 }));
 
 let db = new sqlite3.Database('stats.db', (err) => {
